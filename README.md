bedabbb3392c22e963879e022cede7b5544bcbd9
Bruno Fioritto <bruno.fioritto@jam3.com>
2022-06-15 18:51:53 -0300

    allure report configured

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # webdriverio-cucumber
 webdriverio cucumber template
 
-command line <npx wdio run ./wdio.conf.js> to execute the tests
+command line <npm run wdio> to execute all tests
+command line <npm run test> (modifying the script on package.json first) you will be able to execute a specific feature
+
+ALLURE REPORT
+
+In order to execute the allure report you need to run the tests first and then execute the allure report i.e:
+npm run wdio
+npm run allure-report
--- wdio.conf.js
+++ wdio.conf.js
@@ -133,9 +133,14 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
-
-
+    reporters: ['spec',
+                ['allure', {
+                    outputDir: 'Reports/allure-results',
+                    disableWebdriverStepsReporting: false,
+                    disableWebdriverScreenshotsReporting: false,
+                    useCucumberStepReporter: true,
+                }]
+    ],
     //
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
